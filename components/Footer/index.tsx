b861509c8a2c5c50a421c7755bf8f296106136bb
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-06 17:11:36 +0100

    component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footercontent">
+            <div className="footerillustration"><img src="images/footerImage.png" loading="lazy" width="337" sizes="(max-width: 479px) 100vw, 337px" srcSet="images/footerImage-p-500.png 500w, images/footerImage-p-800.png 800w, images/footerImage-p-1080.png 1080w, images/footerImage.png 1131w" alt="" className="footerimage"/></div>
+            <div className="footerlinks">
+                <h4 className="footertitle">Quick Links</h4>
+                <div className="looterlinkswrapper">
+                <div className="footerlinkslist">
+                    <a href="#" className="footerlinkitem">Home</a>
+                    <a href="#" className="footerlinkitem">Gallery</a>
+                    <a href="#" className="footerlinkitem">Blog</a>
+                    <a href="#" className="footerlinkitem">Event</a>
+                </div>
+                <div className="footerlinkslist">
+                    <a href="#" className="footerlinkitem">Store</a>
+                    <a href="#" className="footerlinkitem">Pay Dues</a>
+                    <a href="#" className="footerlinkitem">Start Voting</a>
+                </div>
+                </div>
+            </div>
+            <div className="footercontact">
+                <h4 className="footertitle">Contact</h4>
+                <div className="looterlinkswrapper">
+                <div className="footerlinkslist">
+                    <div className="contacttext">Nwabueze Okenna<br/>President<br/>0816-728-392</div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div className="copyright">
+            <h3 className="copyrighttext">Copyright NACOS 2021</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="navbar"><img src="images/logo.png" loading="lazy" width="73" alt="" className="navbarlogo"/>
+                <div data-w-id="4145a70e-099e-d4d3-af9c-41837d4fc758" className="handbugur mobile">
+                <div className="line _1 componet"></div>
+                <div className="line _2"></div>
+                <div className="line _3"></div>
+            </div>
+            <div data-w-id="4145a70e-099e-d4d3-af9c-41837d4fc75c" className="handbugur fullscreen">
+                <div className="line _1 componet"></div>
+                <div className="line _2"></div>
+                <div className="line _3"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
