a5f5d95f854124a7746f0e0f4602a3a64c6c2edc
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-08 10:18:42 +0100

    Blog Item and blogList

---
+++ components/Blogs/BlogItem.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BlogItem = () => {
+    return (
+        <div data-w-id="cdcfd309-e48b-a6e7-b265-556ba07f164c" className="blogitem "><img src="images/blogImage.png" loading="lazy" width="345" alt="" className="blogimage"/>
+            <div className="blogcontent">
+                <h1 className="blogtitle">Application for Advertised Course/ Staff form Request</h1>
+                <div className="text-block-2">17th June 2021</div>
+                <div className="blogdescription">This is some text inside of a dLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt adipiscing elit, sed do eiusmod tempor incididunt .iv block.</div>
+            </div>
+        </div>
+    );
+}
+
+export default BlogItem;
---
+++ components/Blogs/BlogList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BlogItem from './BlogItem';
+
+const BlogList = ({}) => {
+    return (
+        <div className="blogwrapper">
+            <BlogItem/>
+            <BlogItem/>
+            <BlogItem/>
+        </div>
+    );
+}
+
+export default BlogList;
