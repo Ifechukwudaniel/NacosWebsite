8a39ba84ae67006d9cbe02b8407255839e51c8f4
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-15 21:42:30 +0100

    dues form

--- components/PayDues/DuesForm.tsx
+++ components/PayDues/DuesForm.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { HtmlHTMLAttributes, useState, useEffect } from 'react';
+
 
 const DuesForm = () => {
+    const  [ section , setSection ] = useState<string>()
+    const [ sectionList , setSectionsList ] = useState<string[]>([])
+
+    useEffect(() => {
+        let currentYear =   new Date().getFullYear()+1
+        let allYears =  [0,1, 2,3,].map(x=>currentYear -x)
+        let sectionStrings : string[] = allYears.map((x, i)=> `${x}/${allYears[i+1] ? allYears[i+1] : (allYears[i])-1 }`)
+        setSectionsList(sectionStrings)
+    }, []);
+
+    const handleSectionChange :React.ChangeEventHandler<HTMLSelectElement> = (event)=>{
+        setSection(event.target.value);
+    }
+
+    const handleSubmit = (e:React.SyntheticEvent) =>{
+        e.preventDefault();
+        console.log(section)
+    }
+
     return (
         <div className="payduesformwrapper w-form">
-            <form id="email-form" name="email-form" data-name="Email Form" className="payduesform">
+            <form onSubmit={handleSubmit} className="payduesform">
                 <label className="profilelabel">Session</label>
-                    <select id="field" name="field" className="profileinput payduesinput w-select">
+                    <select onChange={handleSectionChange}  name="section" className="profileinput payduesinput w-select">
                         <option value="">Select Section ...</option>
-                        <option value="2018/2019">2018/2019</option>
+                        {
+                            sectionList.map((x, i)=>(
+                                <option key = {i} value={`${x}`}>{x}</option>
+                            ))
+                        }
                     </select>
                 <input type="submit" value="Make Payment" data-wait="Please wait..." className="profilesubmitbutton w-button"/>
             </form>
