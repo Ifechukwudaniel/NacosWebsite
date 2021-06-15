5ca599fcd738400d56e61d874d9bd9fc15108a5f
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-15 21:42:16 +0100

    dues set up

--- pages/payDues/index.tsx
+++ pages/payDues/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react';
 import Footer from '../../components/Footer';
 import CustomHeader from '../../components/Header/CustomHeader';
 import DuesForm from '../../components/PayDues/DuesForm';
@@ -8,6 +9,7 @@ import DuesItem from '../../components/PayDues/DuesItem';
 // import styles from '../styles/Home.module.css'
 
 export default function PayDuesPage() {
+  const [hasPreviousPayments, setHasPreviousPayments] = useState<Boolean>(false);
     return (
         <div>
             <CustomHeader/>
@@ -16,21 +18,23 @@ export default function PayDuesPage() {
             </div>
             <div className="paydues">
                 <DuesForm/>
-                <div className="profilepagecontainer">
+                {
+                hasPreviousPayments && (
+                    <div className="profilepagecontainer">
                     <h3 className="profilepagetitle payduestitle previouspayments">Previous Payments</h3>
                 </div>
+                )
+                }
             </div>
             <div className="previouspaymentwrapper">
-                    <div className="previouspayment">
-                        <DuesHeader/>
-                        <DuesItem section="2020/2021" amount="3000" date="2 January 2021"/>
-                        <DuesItem section="2020/2021" amount="3000" date="2 January 2021"/>
-                        <DuesItem section="2020/2021" amount="3000" date="2 January 2021"/>
-                        <DuesItem section="2020/2021" amount="3000" date="2 January 2021"/>
-                        <DuesItem section="2020/2021" amount="3000" date="2 January 2021"/>
-                        <DuesItem section="2020/2021" amount="3000" date="2 January 2021"/>
-                    </div>
-                </div>
+                {
+                    hasPreviousPayments && (
+                            <div className="previouspayment">
+                                <DuesHeader/>
+                                <DuesItem section="2020/2021" amount="3000" date="2 January 2021"/>
+                            </div>
+                    )}
+            </div>
             <Footer/>
         </div>
     )
