edeefd22cfdcb0dc51288950ea3ec7211749bd5b
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-19 14:40:35 +0100

    login portal

--- pages/api/users/loginPortal.ts
+++ pages/api/users/loginPortal.ts
@@ -11,6 +11,7 @@ import {IUser} from '@models/User'
 export default async function loginPortal (req: NextApiRequest, res: NextApiResponse) {
     if(req.method!=="POST") return  res.status(500).send({ status:500, message:" Request was not found"} )
     let {matricNumber, password} :{matricNumber:string , password:string} = req.body
+    matricNumber =matricNumber.toUpperCase();
     if(!matricNumber && matricNumber.length > 0) return res.status(500).send({ status:500, message:"Empty Matric Number"})
     if(!password && password.length > 0) return res.status(500).send({ status:500, message:"Empty Password Filed"} )
     let data = qs.stringify({ '__VIEWSTATE': '/wEPDwUJNTMzNDU1NzA1D2QWAgIDDxYCHgxhdXRvY29tcGxldGUFA29mZmRkUqof2LAdE4XaTKVxV+vLr66d+Qk=','__VIEWSTATEGENERATOR': 'C2EE9ABB','__EVENTVALIDATION': '/wEdAARd9vtBLyjpHiBYSoOme+1hY3plgk0YBAefRz3MyBlTcHY2+Mc6SrnAqio3oCKbxYainihG6d/Xh3PZm3b5AoMQ9QuH9fLNL5w33RiP/YoinnH+MVI=','txtUserName': matricNumber,'txtPassword': password,'btnLogin': 'Login' });
@@ -21,8 +22,9 @@ export default async function loginPortal (req: NextApiRequest, res: NextApiResp
         data:data
     } 
 
-    try {
+    try {  
         await connectToDatabase()
+        let foundUser: IUser =  await  User.findOne({matricNumber:matricNumber.toUpperCase()})
         let loginResponse =  await (await axios(config)).data
         let  $ = cheerio.load(loginResponse)
         let errorMsg = $('#lblErrorMessage').text()
@@ -34,6 +36,7 @@ export default async function loginPortal (req: NextApiRequest, res: NextApiResp
         $ = cheerio.load(responseDetail)
         let department = $("#HeaderContent_txtDepartment").val()
         if(department !=="COMPUTER SCIENCE")  return  res.status(500).send({ status:500, message:"Only Computer Science student can login"} )
+        if(foundUser)   return  res.json({ message:"Success", user: foundUser})
         let name : string = $("#HeaderContent_lblStudentsName").text()
         let level : number =  parseInt($("#HeaderContent_txtLevel").val().toString())
         let phone:string =  $("#HeaderContent_txtPhoneNumber").val().toString()
@@ -43,7 +46,7 @@ export default async function loginPortal (req: NextApiRequest, res: NextApiResp
         let address  = $("#HeaderContent_txtContactAddress").val()
         const newUser: IUser = new User({name, level, phone, email,matricNumber,  userImage:Location, address})
         await newUser.save()
-        return  res.json({ working:true, user: newUser }) 
+        return  res.json({ message:"Success", user: newUser }) 
     } catch (err) {
         res.status(500).send({ status:500, message:err.message} )
     }
