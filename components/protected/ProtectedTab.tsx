587e731a02e69bd54c7906fd2c4823b12824d15c
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-21 06:57:27 +0100

    added material to tab

--- components/protected/ProtectedTab.tsx
+++ components/protected/ProtectedTab.tsx
@@ -10,24 +10,22 @@ const ProtectedTab = () => {
                         <a onClick={(e)=>{
                             e.preventDefault()
                             router.push('/protected/EventAdmin',  null, { shallow: true })
-                        }} className={ router.pathname=== "/protected/EventAdmin" ? "adminhederitem active w--current"  :" adminhederitem"} >Events</a>
-                        
+                        }} className={ router.pathname=== "/protected/EventAdmin" ? "adminhederitem active w--current"  :" adminhederitem"} >Events</a> 
                         <a  onClick={()=>{
                             router.push('/protected/BlogAdmin', null, { shallow: true })
                         }} className={ router.pathname=== "/protected/BlogAdmin" ? "adminhederitem active w--current"  :" adminhederitem"}>Blogs</a>
-
                         <a onClick={()=>{
                             router.push('/protected/VoteAdmin',  null, { shallow: true })
                         }} className={ router.pathname=== "/protected/VoteAdmin" ? "adminhederitem active w--current"  :" adminhederitem"}>Voting</a>
-
                         <a  onClick={()=>{
                             router.push('/protected/DuesAdmin',  null, { shallow: true })
                         }} className={ router.pathname=== "/protected/DuesAdmin" ? "adminhederitem active w--current"  :" adminhederitem"}>Dues</a>
-
+                         <a onClick={()=>{
+                            router.push('/protected/MaterialsAdmin' ,null, { shallow: true })
+                        }} className={ router.pathname=== "/protected/MaterialsAdmin" ? "adminhederitem active w--current"  :" adminhederitem"}>Material</a>
                         <a onClick={()=>{
                             router.push('/protected/AdminGallary' ,null, { shallow: true })
                         }} className={ router.pathname=== "/protected/AdminGallary" ? "adminhederitem active w--current"  :" adminhederitem"}>Gallery</a>
-
                     </div>
         </div>
     );
