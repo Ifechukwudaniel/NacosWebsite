14bdc85ae3e8b44263e5f5eb6a6fe76aab5a9751
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-23 23:23:40 +0100

    Admin Gallary Item

--- components/protected/AdminGallary/GalleryItem.tsx
+++ components/protected/AdminGallary/GalleryItem.tsx
@@ -1,14 +1,25 @@
+import ModalOverlay from '@components/ModalOverlay';
+import ConfirmAlertHook from '@hooks/ConfirmAlertHook';
 import { motion } from 'framer-motion';
 import React from 'react';
+import { useState } from 'react';
+
 
 const GalleryItemAdmin = () => {
+    const [ edit , setEdit] = useState(false)
+
+    const handleDelete =()=>{
+        console.log("ndndn")
+    }
+
     return (
         <div  className="imageitem">
-            <motion.img whileTap={{scale:0.5}} src="/images/dotIcon.png" loading="lazy" alt="" className="dotedimage"/>
+            <ModalOverlay onClose={()=>setEdit(!edit)} title="Edit Gallery" active={edit}/>
+            <motion.img onClick={()=>setEdit(!edit)} whileTap={{scale:0.5}} src="/images/dotIcon.png" loading="lazy" alt="" className="dotedimage"/>
             <img src="/images/galary.png" loading="lazy" alt="" className="imagegalary"/>
             <h5 className="gallarytitle">Getting Scholarship Abroad</h5>
             <div  className="deletebutton w-inline-block">
-                <motion.img whileTap={{scale:0.5}} src="/images/close.png" loading="lazy" alt="" className="deleteicon img"/>
+                <motion.img onClick={()=>ConfirmAlertHook(handleDelete)} whileTap={{scale:0.5}} src="/images/close.png" loading="lazy" alt="" className="deleteicon img"/>
             </div>
         </div>
     );
