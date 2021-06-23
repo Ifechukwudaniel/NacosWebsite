d3d2016b6d3fa4864c3538169f82da563a0c528c
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-23 23:23:12 +0100

    Admin Blog Item

---
+++ components/protected/AdminBlog/BlogAdminItem.tsx
@@ -0,0 +1,32 @@
+import ModalOverlay from '@components/ModalOverlay';
+import ConfirmAlertHook from '@hooks/ConfirmAlertHook';
+import { motion } from 'framer-motion';
+import React from 'react';
+import { useState } from 'react';
+
+
+const BlogAdminItem = () => {
+    const [edit , setEdit] = useState(false)
+
+    const handleClickDelete = ()=>{
+        console.log("delete")
+    }
+
+    return (
+        <div className="blogadminitem">
+            <ModalOverlay onClose={()=>setEdit(!edit)} title="Edit Blog" active={edit}/>
+            <img src="/images/galary.png" loading="lazy" alt="" className="blogadminimage"/>
+            <div style={{marginLeft:30}} className="blogadmintitlewrapper">
+                <h1 className="blogtitle admin">Getting Scholarship Abroad.  The Fastest Way <br/>to Becoming a Scholar</h1>
+            </div>
+            <motion.div onClick={()=>setEdit(!edit)} whileTap={{scale:0.7}}  className="blogadminedit"><img src="/images/edit_1.png" loading="lazy" alt="" className="editicon"/>
+                <div className="edittext">Edit</div>
+            </motion.div>
+            <motion.div whileTap={{scale:0.7}} onClick={()=>ConfirmAlertHook(handleClickDelete)} className="blogadminedit"><img src="/images/delete.png" loading="lazy" alt="" className="editicon"/>
+                <div className="edittext delete">Delete</div>
+            </motion.div>
+        </div>
+    );
+}
+
+export default BlogAdminItem;
