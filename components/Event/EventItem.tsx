c522b880657a7c6bd16c83aa61155bb496b8c28b
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-24 12:05:01 +0100

    changes

--- components/Event/EventItem.tsx
+++ components/Event/EventItem.tsx
@@ -1,8 +1,10 @@
 import { motion , useAnimation } from 'framer-motion';
+import moment from 'moment';
 import React, { useState, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
-const EventItem = (props: {date :Number,month :string, title:string,  description: string }) => {
+
+const EventItem = (props:{title? :string , date ?: Date , id ?:string, description? : string}) => {
   const controls = useAnimation();
   const { ref,inView, entry } = useInView({
       threshold:[0, 1],
@@ -26,8 +28,8 @@ const EventItem = (props: {date :Number,month :string, title:string,  descriptio
     return (
         <motion.div className="eventitem eventpage">
           <div className="eventdate">
-            <h4 className="dateinmonth">{props.date}</h4>
-            <h4 className="dateinmonth dateday">{props.month}</h4>
+            <h4 className="dateinmonth">{moment(props.date).date()}</h4>
+            <h4 className="dateinmonth dateday">{moment(props.date).format("MMM")}</h4>
           </div>
           <div className="eventcontent">
             <h3 className="eventti">{props.title}</h3>
--- components/material/materialItem.tsx
+++ components/material/materialItem.tsx
@@ -1,12 +1,14 @@
+import download from '@utils/download';
 import { motion } from 'framer-motion';
 import React from 'react';
 
-const MaterialItem = () => {
+const MaterialItem = (props: {title:string ,fileLink:string}) => {
     return (
         <div className="materialitem">
-            <h3 className="materialtitle">CSC 141 2020 Past Question</h3>
+            <h3 className="materialtitle">{props.title} </h3>
             <motion.div whileTap={{scale:0.8}} className="dowloadwrppaer">
-                <img src="/images/download-1.svg" loading="lazy" width="70" alt="" className="dowloadicon"/>
+
+                <img src="/images/download-1.svg" onClick={()=>download(props.fileLink,props.title)} loading="lazy" width="70" alt="" className="dowloadicon"/>
             </motion.div>
         </div>
     );
