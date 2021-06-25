6487b667b1bdacceee6e41e72ac455f785ad549c
Angry Coding <mailidanielifechukwu@gmail.com>
2021-06-25 14:07:33 +0100

    changes

--- pages/protected/MaterialsAdmin.tsx
+++ pages/protected/MaterialsAdmin.tsx
@@ -1,27 +1,57 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
-// import styles from '../styles/Home.module.css'
-import { signIn, signOut, useSession } from 'next-auth/client'
 import { useState } from 'react'
-import LoadingOverlay from '@components/LoadingOverlay'
 import { useRouter } from 'next/router'
-import { isMobile } from 'react-device-detect'
-import { Fragment } from 'react'
 import CustomHeader from '@components/Header/CustomHeader'
 import ProtectedTab from '@components/protected/ProtectedTab'
 import AdminMaterialItem from '@components/protected/AdminMaterial/AdminMaterialItem'
+import { Material, IMaterial } from '@models/Material'
+import axios from 'axios'
+import ModalOverlay from '@components/ModalOverlay'
+import MaterialForm from '@components/protected/AdminMaterial/MaterialForm'
+import {NotificationManager} from 'react-notifications';
 
-export default function MaterialAdminPage() {
-    const [ password , setPassword] = useState<string>("")
+export default function MaterialAdminPage(props:{materials: IMaterial[]}) {
+    const [pageMaterial,setPageMaterial] = useState(props.materials)
+    const [ loadingText, setLoadingText] = useState<string>("")
     const [loading, setLoading] = useState(false)
+    const [open, setOpen] = useState(false)
     const router = useRouter()
-    
 
+    const handleCreate =  async (data)=>{
+        try {
+            setLoadingText("Creating Material")
+            setLoading(true)
+            data.fileLink ="nsnx"
+            await axios.post(`http://localhost:3000/api/material/createMaterial`,{
+                title:data.title, base64File:data.file,fileLink:"jjj"
+            })
+            setPageMaterial([...pageMaterial, data])
+            NotificationManager.success("Created Event");
+            return setOpen(!open)
+        } catch (error) {
+            setLoading(false)
+        }
+    }
+
+    const handleDelete = async (id)=>{
+        try {
+            setLoadingText("Deleting Material"), setLoading(true)
+            await axios.post(`http://localhost:3000/api/material/delete`,{id})
+            setPageMaterial(pageMaterial.filter(x=>x._id!==id))
+            return NotificationManager.success("Deleted Material");
+        } catch (error) {
+            setLoading(false)
+        }
+    }
     return (
         <div>
             <CustomHeader/>
             <ProtectedTab/>
+            <ModalOverlay onClose = {()=>setOpen(!open)} active={open} title="Add Materials">
+                <MaterialForm handleSubmit={handleCreate} />
+            </ModalOverlay>
             <div className="duescontent">
                 <div className="w-form">
                     <form id="email-form" name="email-form" data-name="Email Form" className="serchduesform">
@@ -29,12 +59,29 @@ export default function MaterialAdminPage() {
                         <motion.div   whileTap={{scale:0.9}} className="searchrecods icon"><img src="/images/search.png" loading="lazy" width="16" alt="" className="searchiocn"/></motion.div>
                     </form>
                 </div>
+                <div style={{width:'80%', margin:"auto"}} className="addimagewrapper">
+                    <div className="blogadmintitlewrapper">
+                        <h1 className="editadmintitle">Current Materials</h1>
+                    </div>
+                    <motion.a  onClick={(e)=>{e.preventDefault(), setOpen(!open)}} whileHover={{opacity:0.8}} whileTap={{scale:1.1}}  className="createitembutton addimagebutton w-button">Add Material's<br/></motion.a>
+                </div>
             </div>
             <div className="materiallistwrapper">
                 <div className="materiallist">
-                    <AdminMaterialItem/>
+                    {
+                        pageMaterial.map((x,i)=>(
+                            <AdminMaterialItem onDelete={handleDelete} key={i} _id={x._id}  {...x}/>
+                        ))
+                    }
                 </div>
             </div>
         </div> 
     )
+}
+
+export async function getStaticProps() {
+        const materials: IMaterial[] = await (await axios(`${process.env.URL}/api/material`)).data
+        return {
+            props: {materials},
+        };
 }
